5c6e979a77384390f4ff3f61e19b9ff05154c452
JuanBC <juanblasmdq@gmail.com>
2021-07-15 03:04:59 -0300

    Log form: in progress

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from typing import TextIO
 import zipfile
 from pathlib import Path
 from configparser import ConfigParser
+import tkGUI #project-specific module
 
 __author__ = 'Juan Blas Carelli'
-__version__ = 'July 14, 2021'
+__version__ = 'July 15, 2021'
 
 class ZipUtilities:
     def __init__(self):
@@ -27,7 +28,7 @@ class ZipUtilities:
             short_path= self.zipFileArcName(filetozip,str(Path(filetozip).parent))     
             zip_file.write(filetozip, arcname= short_path)
         else:
-            print('Root directory:')
+            #print('Root directory:')
             self.addFolderToZip(zip_file, filetozip)
         zip_file.close()
     
@@ -65,29 +66,50 @@ def main():
     DEFAULT_BACKUP_PATH = r'c:/_baks'
     DEFAULT_INPUT_PATH = r'c:/_filesToZip' # r'' (raw string) indicate that special characters as \U should not be evaluated
 
-    use_config = input('Use information included in "config.ini"? [y=yes] (type anything if you don´t know what this means): ').lower()
+    #use_config = input('Use information included in "config.ini"? [y=yes] (type anything to run GUI): ').lower()
+    use_config = 'no'
 
     if use_config == 'y':
         print('Using "config.ini". If any path is empty, default paths will be considered.')
         BACKUP_PATH = get_config('BACKUP_PATH')
         INPUT_PATH = get_config('INPUT_PATH')
     else:
-        BACKUP_PATH = input('Write destination path. If empty, default path "{}" will be considered: '.format(DEFAULT_BACKUP_PATH))
-        INPUT_PATH = input('Write path to zip. If empty, default path "{}" will be considered: '.format(DEFAULT_INPUT_PATH)).strip('"')
+        #GUI from tkGUI
+        label_list = ['Destination path (empty=Default)','Path to zip (empty=Default)']
+        logContent = ''
+        GUI_extraction = tkGUI.ShowForm('Backup creator',label_list,'')
+        
+
+        BACKUP_PATH = GUI_extraction[0]
+        INPUT_PATH = GUI_extraction[1]
+
+        #CLI input - replaced by GUI on June 15, 2021
+        #BACKUP_PATH = input('Write destination path. If empty, default path "{}" will be considered: '.format(DEFAULT_BACKUP_PATH))
+        #INPUT_PATH = input('Write path to zip. If empty, default path "{}" will be considered: '.format(DEFAULT_INPUT_PATH)).strip('"')
         if not BACKUP_PATH:
-            print('\n>> Using default destination path "{}"'.format(DEFAULT_BACKUP_PATH))
-            input()
+            logContent = logContent + '\n' + '>> Using default destination path "{}"'.format(DEFAULT_BACKUP_PATH)
+            # GUI_log.setLogValue(logContent)
+            # GUI_log.update_log()
+
+
+            #log.WriteToLog(logContent)
+            #print('\n>> Using default destination path "{}"'.format(DEFAULT_BACKUP_PATH))
+            #input()
             BACKUP_PATH = DEFAULT_BACKUP_PATH
 
         if not INPUT_PATH:
-            print('\n>> Using default path to zip "{}"'.format(DEFAULT_INPUT_PATH))
-            input()
+            logContent = logContent + '\n' +'>> Using default path to zip "{}"'.format(DEFAULT_INPUT_PATH)
+            #print('\n>> Using default path to zip "{}"'.format(DEFAULT_INPUT_PATH))
+            #input()
             INPUT_PATH = DEFAULT_INPUT_PATH
 
     INPUT_PATH=INPUT_PATH.split(',')
     for in_path in INPUT_PATH:
         in_path = r'{}'.format(in_path).strip()
-        print('\n>> !WORKING ON PATH {}'.format(in_path))
+
+        #print('\n>> !WORKING ON PATH {}'.format(in_path))
+        logContent = logContent + '\n' +'>> !WORKING ON PATH {}'.format(in_path)
+
         Path(BACKUP_PATH).mkdir(parents=True, exist_ok=True) # Make dir if dir not exist
         folder_to_back = Path(in_path).name
         date = datetime.now().strftime('%Y-%m-%d')
@@ -98,12 +120,24 @@ def main():
         utilities = ZipUtilities()
         utilities.toZip(in_path, back_full_name)
 
-        print('\n>> Zip file created at "{}"'.format(BACKUP_PATH))
-        print('>> Zip full name == "{}"'.format(back_full_name))
-        
-        
-    print('\n>> END OF PROCESS. Press any key to exit')
-    input()
+        #print('\n>> Zip file created at "{}"'.format(BACKUP_PATH))
+        logContent = logContent + '\n' +'>> Zip file created at "{}"'.format(BACKUP_PATH)
+        #print('>> Zip full name == "{}"'.format(back_full_name))
+        logContent = logContent + '\n' +'>> Zip full name == "{}"'.format(back_full_name)
 
+        
+    #print('\n>> END OF PROCESS. Press any key to exit')
+    #input()
+
+    logContent = str(logContent + '\n' +'>> END OF PROCESS. Close window to exit')
+
+    # GUI_log = tkGUI.LogWindow()
+    # GUI_log.setLogValue(logContent)
+    # GUI_log.update_log
+
+    print(logContent)
+    GUI_log = tkGUI.ShowLog(logContent)
+
+    
 if __name__ == '__main__':
     main()
